1016da57684a08edf032061b8836065ceee9249b
dahrif <dahri.fatima.dev@gmail.com>
2021-12-14 10:41:08 +0100

    change repository

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+const userData = mongoose.model("user", userSchema);
+
+module.exports = userData;
---
+++ src/app/productlist/product.model.ts
@@ -0,0 +1,8 @@
+export class ProductModel {
+    constructor(
+
+        public productName : number,
+        public productEmail : string,
+        public productPassword : string
+    ){}
+}
